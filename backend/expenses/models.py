c527050cee615015ff4ac55b7ad381e51c2f989a
KendallBG <kendall.barboza.garcia@gmail.com>
2025-12-15 22:37:25 -0600

    Add Expense model with basic fields

--- backend/expenses/models.py
+++ backend/expenses/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Expense (models.Model):
+    title = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=50)
+    date = models.CharField()
+    created_at = models.DateTimeField(auto_created=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.amount}"
